0a871585848e938bcbd2474665450030a71545bd
HMS <harrisonsbennett@gmail.com>
2019-06-27 16:58:43 +0100

    multiple rules

--- vmsetup.tf
+++ vmsetup.tf
@@ -29,20 +29,43 @@ resource "azurerm_network_security_group" "main" {
 	name = "${var.prefix}-NSG"
 	location = "${azurerm_resource_group.main.location}"
 	resource_group_name = "${azurerm_resource_group.main.name}"
-}
 
-resource "azurerm_network_security_rule" "main" {
-	name = "SSH"
-	priority = 100
-	direction = "Outbound"
-	access = "Allow"
-	protocol = "Tcp"
-	source_port_range = "*"
-	destination_port_range = "22"
-	source_address_prefix = "*"
-	destination_address_prefix = "*"
-	resource_group_name = "${azurerm_resource_group.main.name}"
-	network_security_group_name = "${azurerm_network_security_group.main.name}"
+	security_rule {
+		name = "SSH"
+        	priority = 100
+        	direction = "Outbound"
+        	access = "Allow"
+        	protocol = "Tcp"
+        	source_port_range = "*" 
+        	destination_port_range = "22"
+        	source_address_prefix = "*"
+        	destination_address_prefix = "*"
+	}
+
+	security_rule {
+                name = "HTTPS"
+                priority = 150
+                direction = "Outbound"
+                access = "Allow"
+                protocol = "Tcp"
+                source_port_range = "*"
+                destination_port_range = "8080"
+                source_address_prefix = "*"
+                destination_address_prefix = "*"
+        }
+
+	security_rule {
+                name = "Python"
+                priority = 200
+                direction = "Outbound"
+                access = "Allow"
+                protocol = "Tcp"
+                source_port_range = "*"
+                destination_port_range = "8088"
+                source_address_prefix = "*"
+                destination_address_prefix = "*"
+        }
+
 }
 
 resource "azurerm_public_ip" "main" {
